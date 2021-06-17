f858b337648f2bb4a664e2f5dc31997c22590f65
BTC <briantclark@gmail.com>
2021-06-17 10:39:31 -0400

    remove test text

--- cmd/gpio.c
+++ cmd/gpio.c
@@ -224,8 +224,6 @@ static int do_gpio(cmd_tbl_t *cmdtp, int flag, int argc, char * const argv[])
 		}
 		gpio_direction_output(gpio, value);
 	}
-	
-	test
 		
 	printf("gpio: pin %s (gpio %u) value is ", str_gpio, gpio);
 
