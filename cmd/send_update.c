1a7b0d71e323a128d0bf6036cbee66c66f446b9f
BTC <briantclark@gmail.com>
2021-06-21 10:01:03 -0400

    add headers

--- cmd/send_update.c
+++ cmd/send_update.c
@@ -4,6 +4,22 @@ Send update packages from one device to another
 B. Clark
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+#include <netdb.h>
+#include "packet.h"
+#include "types/error_packet.h"
+
+rftr
+
 #include <common.h>
 #include <command.h>
 #include <errno.h>
