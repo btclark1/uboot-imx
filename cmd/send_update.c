025ad71dfd3267717c2a63ee01902f15c5d6a62a
BTC <briantclark@gmail.com>
2021-06-21 10:07:17 -0400

    added more headers

--- cmd/send_update.c
+++ cmd/send_update.c
@@ -5,6 +5,16 @@ B. Clark
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+#include <netdb.h>
 
 
 #include <common.h>
